c1c61336cfd3c319bccce1fdea967f2b7a64d6e5
juhom205 <80626044+juhom205@users.noreply.github.com>
2022-01-23 13:18:34 +0100

    Add files via upload

--- right-menu-config.js
+++ right-menu-config.js
@@ -1,7 +1,7 @@
 module.exports = [
-    {role : 'reload'},
-    {role : 'cut'},
-    {role : 'copy'},
-    {role : 'paste'},
-    {role : 'redo'},
-]
+  { role: "reload" },
+  { role: "cut" },
+  { role: "copy" },
+  { role: "paste" },
+  { role: "redo" },
+];
