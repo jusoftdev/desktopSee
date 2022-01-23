72edab8b11072697707de88b35bf1a5d3affb290
juhom205 <80626044+juhom205@users.noreply.github.com>
2022-01-23 13:18:46 +0100

    Add files via upload

--- preload.js
+++ preload.js
@@ -1,29 +1,43 @@
-const {ipcRenderer} = require('electron')
-const appConfig = require('./config')
+const { ipcRenderer } = require("electron");
+const appConfig = require("./config");
 
-window.addEventListener('DOMContentLoaded', () => {
-
-    if (appConfig.hideElementsId.length > 0) {
-        for (let elementsId = 0; elementsId < appConfig.hideElementsId.length; elementsId++) {
-            let hiddenElementsId = document.getElementById(appConfig.hideElementsId[elementsId])
-            hiddenElementsId ? hiddenElementsId.style.display = 'none' : console.log
-        }
+window.addEventListener("DOMContentLoaded", () => {
+  if (appConfig.hideElementsId.length > 0) {
+    for (
+      let elementsId = 0;
+      elementsId < appConfig.hideElementsId.length;
+      elementsId++
+    ) {
+      let hiddenElementsId = document.getElementById(
+        appConfig.hideElementsId[elementsId]
+      );
+      hiddenElementsId
+        ? (hiddenElementsId.style.display = "none")
+        : console.log;
     }
+  }
 
-    if (appConfig.hideElementsClass.length > 0) {
-        for (let elementsClass = 0; elementsClass < appConfig.hideElementsClass.length; elementsClass++) {
-            let hiddenElementsClass = document.getElementsByClassName(appConfig.hideElementsClass[elementsClass])
-            hiddenElementsClass ? hiddenElementsClass[0].style.visibility = 'hidden' : console.log
-        }
+  if (appConfig.hideElementsClass.length > 0) {
+    for (
+      let elementsClass = 0;
+      elementsClass < appConfig.hideElementsClass.length;
+      elementsClass++
+    ) {
+      let hiddenElementsClass = document.getElementsByClassName(
+        appConfig.hideElementsClass[elementsClass]
+      );
+      hiddenElementsClass
+        ? (hiddenElementsClass[0].style.visibility = "hidden")
+        : console.log;
     }
+  }
 
-    document.title = appConfig['appName']
+  document.title = appConfig["appName"];
 
-    let tryagainbtn = document.getElementById("tryagain")
-    tryagainbtn ? tryagainbtn.onclick = runc : false
+  let tryagainbtn = document.getElementById("tryagain");
+  tryagainbtn ? (tryagainbtn.onclick = runc) : false;
 
-    function runc() {
-        ipcRenderer.send('online-status-changed', true)
-    }
-
-})
+  function runc() {
+    ipcRenderer.send("online-status-changed", true);
+  }
+});
